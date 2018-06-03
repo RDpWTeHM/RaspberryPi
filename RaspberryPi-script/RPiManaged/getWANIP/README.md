799c97c17df71d5930e45b5ece0bb2819f4fb94e
Ace Lin <echislin123@live.com>
2018-06-03 16:40:07 +0800

    add script of running to get WAN IP.

---
+++ RaspberryPi-script/RPiManaged/getWANIP/README.md
@@ -0,0 +1,33 @@
+# Developer Documents
+
+## packages depends on
+
+requests; bs4；
+
+### Install packages
+
+```shell
+$
+$ sudo pip3 install requests
+$ sudo pip3 install beautifulsoup4
+$
+```
+
+
+
+## run getWANIP.py
+
+```shell
+$
+$ wget https://github.com/RDpWTeHM/RaspberryPi/tree/master/RaspberryPi-script/getWANIP/getWANIP.py
+$ chmod +x getWANIP.py
+$ getWANIP.py 
+## get HTML text OK!
+## 153.35.***.***
+$
+```
+
+
+
+
+
---
+++ RaspberryPi-script/RPiManaged/getWANIP/getWANIP.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+
+#
+#
+#
+
+###
+### import package:
+###
+import requests
+
+from bs4 import BeautifulSoup
+import bs4
+
+import os
+import sys
+
+import spiderutils
+
+###
+### global variables:
+###
+
+###
+### funciton define:
+###
+def getHTMLText():
+	kv = { 'wd':'IP' }
+	r = requests.get("http://www.baidu.com/s", params=kv )
+	r.encoding = r.apparent_encoding
+
+	return r.text
+#fed function
+
+def getIPstr( srcIP ):
+	dst = ''
+	for i in range( len(srcIP) ):
+		if srcIP[i].isdigit() or srcIP[i]=='.' :
+			dst = dst + srcIP[i]
+		else:
+			pass
+	#rof
+	return dst
+#fed function
+
+
+def main():
+	#htmlData = getHTMLText()
+	kv = { 'wd':'IP' }
+	htmlData = spiderutils.getHTMLText("http://www.baidu.com/s", Params=kv)
+	#print (htmlData)
+	print ("get HTML text OK!")
+
+	## -- 提取body：
+	bodyData = htmlData[htmlData.find("<body"):htmlData.find("</body")]
+	bodyData = '<html><head></head>' + bodyData + '</body></html>'
+	## 这里可以输出 bodyData 一下
+	
+	BodySoup = BeautifulSoup( bodyData, "html.parser" )
+	
+	## -- 提取主要段落：
+	result = BodySoup.find_all( 'div', attrs='result-op c-container' )
+	# print( type(result) )
+	
+	result = list(result)  # turn set to list type
+	RequestsResult = result[0]
+	
+	Str = "%s" % RequestsResult.span
+	# print (Str)
+	## >>> <span class="c-gap-right">本机IP: 1x2.1x3.1x5.2xx</span>
+	
+	## 注意，
+	## Str 必须是正确的值才好进入下一步的字符串处理。
+	## 这边是在解释器模式下的测试+代码记录在CSDN上，
+	## 所以实际情况有一定的可能性不会那么刚好在第一个span标签就是上面所示的字符串
+
+	## 提取 IP 值：
+	ipSoup = BeautifulSoup ( Str, "html.parser" )
+	
+	ipStrKV = ipSoup.span.string
+	# print ipStrKV
+	## 本机IP: 1x2.1x3.1x5.2xx
+	## >>> ipStrKV
+	## '本机IP:\xa01x2.1x3.1x5.2xx'
+	## >>>
+	##
+	
+	( ipKey, ipValue) = ipStrKV.split(":") 	
+
+	ipStr = getIPstr( ipValue )
+	print ( ipStr )
+
+	sys.exit(0)
+#fed main function
+
+
+
+if __name__ == "__main__":
+	main()
+#fi
+
